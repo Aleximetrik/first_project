a9778cb5bc9e8507003300e309e709dd2bc9db46
Alexander Sivoborod <sivoborod@ya.ru>
2026-04-11 15:23:44 +0300

    New function in main.py added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print('Hello from repository!')
+
+import os
+from dotenv import load_dotenv
+
+def print_author():
+    # Загружаем переменные из .env в окружение
+    load_dotenv()
+    # Читаем значение переменной AUTHOR
+    author = os.getenv("AUTHOR")
+    # Выводим результат
+    print(f"Автор проекта: {author}")
+
+if __name__ == "__main__":
+    print_author()
